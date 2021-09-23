a9009fc163ddef136bac016def5965c81f7adaa2
Alan Contreras <alanrcontrerasm@gmail.com>
2021-09-23 12:10:07 -0700

    Add Swipe Listeners and  Paint Strike through functions, gravity, etc.

--- app/src/main/java/com/example/a2_viewdemo/CustomTouchListener.java
+++ app/src/main/java/com/example/a2_viewdemo/CustomTouchListener.java
@@ -20,6 +20,8 @@ public class CustomTouchListener implements View.OnTouchListener{
     }
 
     public class CustomOnGestureListener extends GestureDetector.SimpleOnGestureListener{
+        private static final int SWIPE_DISTANCE_THRESHOLD = 10;
+        private static final int SWIPE_VEL_THRESHOLD = 50;
         @Override
         public void onLongPress(MotionEvent e) {
             Log.d("GESTUREDEMO", "Long Tap Confirmed...");
@@ -29,7 +31,37 @@ public class CustomTouchListener implements View.OnTouchListener{
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return super.onFling(e1, e2, velocityX, velocityY);
+            float distX = e2.getX() - e1.getX(); //x offset for the fling
+            float distY = e2.getY() - e1.getY(); //Y offset for the fling
+
+            Log.d("SWIPE", "Entered on fling: distX, DistY, VelX, Vel Y is..."
+                + distX + distY + velocityX + velocityY);
+
+            if(Math.abs(distX) > Math.abs(distY)
+                && Math.abs(distX) > SWIPE_DISTANCE_THRESHOLD
+                && Math.abs(velocityX) > SWIPE_VEL_THRESHOLD){
+                //this means a horizontal swipe has ocurred
+
+                if(distX > 0){
+                    onSwipeRight();
+                }else{
+                    onSwipeLeft();
+                }
+                return true;
+            }else if(Math.abs(distY) > Math.abs(distX)
+                        && Math.abs(distY) > SWIPE_DISTANCE_THRESHOLD
+                        && Math.abs(velocityY) > SWIPE_VEL_THRESHOLD){
+                //a vertical swipe occurred
+                if(distY >0){
+                    onSwipeDown();
+                }else{
+                    onSwipeUp();
+                }
+                return true; //swipe has occurred
+
+            }
+            return false;
+            //return super.onFling(e1, e2, velocityX, velocityY);
         }
 
         @Override
@@ -52,6 +84,23 @@ public class CustomTouchListener implements View.OnTouchListener{
             onSingleClick();
             return super.onSingleTapConfirmed(e);
         }
+    }
+
+    public void onSwipeUp() {
+        Log.d("GESTUREDEMO+SWIPE", "Swiped Up method inside custom touch listener");
+    }
+
+    public void onSwipeDown() {
+        Log.d("GESTUREDEMO+SWIPE", "Swiped Down method inside custom touch listener");
+    }
+
+    //must be public!!!
+    public void onSwipeLeft() {
+        Log.d("GESTUREDEMO+SWIPE", "Swiped Left method inside custom touch listener");
+    }
+
+    public void onSwipeRight() {
+        Log.d("GESTUREDEMO+SWIPE", "Swiped Right method inside custom touch listener");
     }
 
     public void onLongClick(){
--- app/src/main/java/com/example/a2_viewdemo/MainActivity.java
+++ app/src/main/java/com/example/a2_viewdemo/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.a2_viewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtViewWelcomeMsg;
     ImageView imgViewSample;
     Button btnShowTextOrImgae;
     Button btnShowBoth;
+    boolean bigger = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,23 +63,119 @@ public class MainActivity extends AppCompatActivity {
 
         txtViewWelcomeMsg.setOnTouchListener(new CustomTouchListener(this){
             @Override
+            public void onSwipeUp() {
+                super.onSwipeUp();
+                //I need to change vertical gravity to TOP while zeeping horz gravity as is
+                int horzGravity = txtViewWelcomeMsg.getGravity() & Gravity.HORIZONTAL_GRAVITY_MASK; //getting horz gravity
+                txtViewWelcomeMsg.setGravity(Gravity.TOP | horzGravity);
+            }
+
+            @Override
+            public void onSwipeDown() {
+                super.onSwipeDown();
+                //I need to change vertical gravity to Bottom while zeeping horz gravity as is
+                int horzGravity = txtViewWelcomeMsg.getGravity() & Gravity.HORIZONTAL_GRAVITY_MASK; //getting horz gravity
+                txtViewWelcomeMsg.setGravity(Gravity.BOTTOM | horzGravity);
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                //I need to change horizontal gravity to LEFT while zeeping horz gravity as is
+                int vertGravity = txtViewWelcomeMsg.getGravity() & Gravity.VERTICAL_GRAVITY_MASK; //getting horz gravity
+                txtViewWelcomeMsg.setGravity(Gravity.LEFT | vertGravity);
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                //I need to change horizontal gravity to Right while zeeping horz gravity as is
+                int vertGravity = txtViewWelcomeMsg.getGravity() & Gravity.VERTICAL_GRAVITY_MASK; //getting horz gravity
+                txtViewWelcomeMsg.setGravity(Gravity.RIGHT | vertGravity);
+            }
+
+            @Override
             public void onLongClick() {
+
                 super.onLongClick();
+                Log.d("GESTUREDEMO", "Detected loooong click on the TextView");
+                Toast.makeText(context, "Hi people", Toast.LENGTH_LONG).show();
+                if (txtViewWelcomeMsg.getPaint().isStrikeThruText()){
+                    //bitwise anding the current paint flags with the complement of strike thru flag
+                    //removes the strike thru
+                    //while keeping all other flas as is.
+                    txtViewWelcomeMsg.setPaintFlags(txtViewWelcomeMsg.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+                }else{
+                    //bitwise or of current paint flafs with strike thru flags
+                    //adds the strike thru while keeping all other flas as is.
+                    txtViewWelcomeMsg.setPaintFlags(txtViewWelcomeMsg.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                }
             }
 
             @Override
             public void onDoubleClick() {
+
                 super.onDoubleClick();
+                Log.d("GESTUREDEMO", "Detected double click on the TextView");
+                if(!bigger){
+                    txtViewWelcomeMsg.setTextSize(txtViewWelcomeMsg.getTextSize()/getResources().getDisplayMetrics().density + 10); //we basically get dp and then add 10
+                    bigger = true;
+                }else{
+                    txtViewWelcomeMsg.setTextSize(txtViewWelcomeMsg.getTextSize()/getResources().getDisplayMetrics().density - 10);
+                    bigger = false;
+                }
             }
 
             @Override
             public void onSingleClick() {
                 super.onSingleClick();
+                Log.d("GESTUREDEMO", "Detected single click on TextView");
+                if(txtViewWelcomeMsg.getCurrentTextColor() !=
+                        ResourcesCompat.getColor(getResources(), R.color.teal_200, getTheme())){
+                    txtViewWelcomeMsg.setTextColor(ResourcesCompat.getColor(getResources(), R.color.teal_200, getTheme()));
+                }else{
+                    txtViewWelcomeMsg.setTextColor(Color.rgb(255,255,255));
+                }
             }
 
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 Log.d("GESTUREDEMO", "Detected touch on textView");
+                return super.onTouch(view, motionEvent);
+            }
+        });
+
+        imgViewSample.setOnTouchListener(new CustomTouchListener(this){
+            //if this - the current object does not refer to the activity object
+            //you must scope this with DataType - MainActivity.this - means the current object that is
+            //on instance of MainActivity class
+
+
+            @Override
+            public void onDoubleClick() {
+                super.onDoubleClick();
+                if(imgViewSample.getScaleType() != ImageView.ScaleType.FIT_XY){
+                    imgViewSample.setScaleType(ImageView.ScaleType.FIT_XY);
+                }else{
+                    imgViewSample.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                }
+            }
+
+            @Override
+            public void onSingleClick() {
+                super.onSingleClick();
+                //to compare images you have to use ConstantState
+                if(imgViewSample.getDrawable().getConstantState()
+                        != ResourcesCompat.getDrawable(getResources(), R.drawable.bird, getTheme()).getConstantState()){
+                    imgViewSample.setImageResource(R.drawable.bird);
+                }else{
+                    imgViewSample.setImageResource(R.drawable.fire);
+                }
+            }
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.d("GESTUREDEMO", "Detected on Touch on ImageView");
                 return super.onTouch(view, motionEvent);
             }
         });
